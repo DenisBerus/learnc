3dd644cfe99c1392c6d3d1506b95b9fd3bc8dd57
Денис Берс <denisbers@MacBook-Pro-Denis-2.local>
2022-12-07 22:41:37 +0300

    Домашняя работа 2 выполнена

---
+++ DZ2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 1
+/*
+void Vtoroy(int num)
+    {
+        int num2 = num / 10;
+        int num3 = num2 % 10;
+        Console.WriteLine(num3);
+    }
+Console.Write("Напишите число из 3х цифр: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Vtoroy(numA);
+*/
+
+
+// Задача 2
+/*
+void Videlit(int num)
+    {
+       if(num > 99)
+        {
+            while(num > 999)
+                {
+                    int cur = 10;
+                    num = num / cur;
+                    cur = cur * 10;
+                }
+            int result = num % 10;
+            Console.WriteLine($"Третье число {result}");
+        }
+        if(num <100)
+            {
+                Console.WriteLine("нет третьей цифры");
+            }
+    }
+Console.Write("Напишите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Videlit(numA);
+*/
+
+// Задача 3
+/*
+void Week(int num)
+{
+    if(num == 6 || num == 7) Console.WriteLine("Ура! Выходной!");
+    else Console.WriteLine("Нет, будни(");
+}
+Console.Write("Напишите день недели: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Week(numA);
+*/
